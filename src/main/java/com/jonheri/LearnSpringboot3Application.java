b662a7c3b1e99bf20b83f513b3de0c3ef55368b3
JH01439X <joni.herianto@ist.id>
2025-03-26 10:15:34 +0700

    First Commit

---
+++ src/main/java/com/jonheri/LearnSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.jonheri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringboot3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonheri/LearnSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonheri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
